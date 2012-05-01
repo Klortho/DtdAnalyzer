22d9fdf6b880b673636374b56ed44d9d6f551877
Chris Maloney <voldrani@gmail.com>
2012-05-01 16:50:20 -0400

    Add some simple non-JATS tests

---
+++ script/contextmodel.bat
@@ -0,0 +1,17 @@
+:: Windows version of contextmodel.sh
+
+rem if [ ! $1 ]
+rem    then echo "Usage: contextmodel.sh xmlinstance {catalog}"
+rem    exit 1
+rem fi
+
+rem if [ ! $2 ]
+rem    then cat=/pmc/load/catalog/pmc3-catalog.xml
+rem    else cat=$2
+rem fi
+
+rem Catalog not working under Windows yet.
+set CAT=M:\JATS\Sourceforge-nlm-jats\trunk\jatspacks\catalog.xml
+
+java -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser pmctools.ElementContextApplication %1 %CAT%
+
---
+++ setenv.bat
@@ -0,0 +1,7 @@
+rem This script should be sourced while the current working directory is
+rem the directory in which this resides.
+
+set PROJ_HOME=m:\git\DtdAnalyzer
+
+set CLASSPATH=%PROJ_HOME%\bin;C:\Users\maloneyc\bin\XercesJ-2.11.0\xercesImpl.jar;C:\Users\maloneyc\bin\XercesJ-2.11.0\resolver.jar
+set PATH=%PROJ_HOME%\script;%PATH%
