ed0e48d54475fd840c00dc86a2516a702b95c565
Chris Maloney <voldrani@gmail.com>
2012-08-24 22:21:03 -0400

    Changed setenv.sh to assume all jars are in lib directory

--- datadictionary/setenv.sh
+++ datadictionary/setenv.sh
@@ -4,10 +4,10 @@
 PROJ_HOME=`pwd`
 
 CLASSPATH=$PROJ_HOME/bin
-CLASSPATH=$CLASSPATH:/pmc/JAVA/xmlsh_1_1_7/lib/resolver.jar
-CLASSPATH=$CLASSPATH:/pmc/JAVA/saxon6/saxon.jar
-CLASSPATH=$CLASSPATH:/pmc/JAVA/xmlsh_1_1_7/lib/xercesImpl.jar
-CLASSPATH=$CLASSPATH:/pmc/JAVA/jing-20030619/bin/xml-apis.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/../lib/resolver.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/../lib/saxon.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/../lib/xercesImpl.jar
+CLASSPATH=$CLASSPATH:$PROJ_HOME/../lib/xml-apis.jar
 export CLASSPATH
 
 export PATH=$PROJ_HOME/script:$PATH
