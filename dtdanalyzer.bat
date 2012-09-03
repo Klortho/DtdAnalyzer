c187279211f21df9b9bac5c31ea0fcb7169a623e
Chris Maloney <voldrani@gmail.com>
2012-09-02 21:00:11 -0400

    Fancify dtdanalyzer scripts

--- dtdanalyzer.bat
+++ dtdanalyzer.bat
@@ -1,4 +1,15 @@
-rem Batch file to invoke the datadictionary app
+@echo off
+set DTDANALYZER_ROOT=%~p0
+set CP="%DTDANALYZER_ROOT%build"
+call :findjars "%DTDANALYZER_ROOT%lib"
+set LOGCONFIG=file:%DTDANALYZER_ROOT%etc/log4j.properties
+java -cp %CP% -Xmx1G "-Dlog4j.configuration=%LOGCONFIG%" gov.ncbi.pmc.dtdanalyzer.Application %*
+exit /B
 
-java gov.ncbi.pmc.dtdanalyzer.Application %*%
+:findjars
+for %%j in (%1\*.jar) do call :addjar "%%j"
+for /D %%d in (%1\*) do call :findjars "%%d"
+exit /B
 
+:addjar
+set CP=%CP%;%1
