87ab3613284f0c0a95f05f6f790a70060986cde6
Chris Maloney <voldrani@gmail.com>
2012-09-20 18:11:30 -0400

    Work on structured annotations

---
+++ src/gov/ncbi/pmc/dtdanalyzer/SComments.java
@@ -0,0 +1,22 @@
+/*
+ * SComments.java
+ */
+
+package gov.ncbi.pmc.dtdanalyzer;
+
+import java.util.*;
+
+/**
+ * Holds a collection of SComment objects and provides accessor methods.
+ */
+public class SComments {
+    
+    /**
+     * Adds an SComment to the collection
+     *
+     * @param scomment SComment to be added
+     */    
+    public void addSComment( SComment scomment ) {
+    }
+
+}
