ca0d238d7ee8d8b332e33f917f90277e0af76c4a
Chris Maloney <voldrani@gmail.com>
2012-10-02 05:54:25 -0400

    Starting #4, created dtddocumentor scripts

---
+++ dtddocumentor.bat
@@ -0,0 +1,15 @@
+@echo off
+set DTDANALYZER_ROOT=%~dp0
+set CP="%DTDANALYZER_ROOT%build"
+call :findjars "%DTDANALYZER_ROOT%lib"
+set LOGCONFIG=file:%DTDANALYZER_ROOT%etc/log4j.properties
+java -cp %CP% -Xmx1G "-Dlog4j.configuration=%LOGCONFIG%" gov.ncbi.pmc.dtdanalyzer.DtdAnalyzer -x "%DTDANALYZER_ROOT%/xslt/dtddocumentor.xsl" %*
+exit /B
+
+:findjars
+for %%j in (%1\*.jar) do call :addjar "%%j"
+for /D %%d in (%1\*) do call :findjars "%%d"
+exit /B
+
+:addjar
+set CP=%CP%;%1
