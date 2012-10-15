f6ec002f35daf72ea8b5bedf46a647eed21dd76d
Chris Maloney <voldrani@gmail.com>
2012-10-15 11:13:10 -0400

    Add note about default value for --dir

--- src/gov/ncbi/pmc/dtdanalyzer/App.java
+++ src/gov/ncbi/pmc/dtdanalyzer/App.java
@@ -36,27 +36,27 @@ import org.xml.sax.helpers.XMLReaderFactory;
  * demonstration and debugging.
  */
 public class App {
-    
+
     // CONSTANTS
-    
+
     // ID for SAX driver
     public static final String SAX_DRIVER_PROPERTY = "org.xml.sax.driver";
-                                   
+
     // SAX driver implementation
     public static final String SAX_DRIVER_DEFAULT = "org.apache.xerces.parsers.SAXParser";
-                   
+
     // ID for transformer
     public static final String TRANSFORMER_FACTORY_PROPERTY = "javax.xml.transform.TransformerFactory";
-      
+
     // Transformer implementation - default to Saxon 9, per this documentation page:
     // http://www.saxonica.com/documentation/using-xsl/embedding/jaxp-transformation.xml
     public static final String TRANSFORMER_FACTORY_DEFAULT = "net.sf.saxon.TransformerFactoryImpl";
-         
+
     // Path to reach OASIS dtd
     public static final String OASIS_DTD = "/org/apache/xml/resolver/etc/catalog.dtd";
-                       
+
     // Public id of Oasis DTD
-    public static final String OASIS_PUBLIC_ID = "-//OASIS/DTD Entity Resolution XML Catalog V1.0//EN";  
+    public static final String OASIS_PUBLIC_ID = "-//OASIS/DTD Entity Resolution XML Catalog V1.0//EN";
 
     // Stores the actual command line arguments used, this should be passed in directly from main()
     String[] args;
@@ -64,20 +64,20 @@ public class App {
     // Value of the DTDANALYZER_HOME system property
     private File home;
 
-    
+
     // This is the Options object that gives the list of all of the command-line options
     // that are in effect for this particular invokation.  (The list will differ depending on the
     // driver class; i.e. DtdAnalyzer, DtdDocumentor, etc.)
     private Options activeOpts = new Options();
-    
+
     // This holds the superset of all possible options for any applications here.  Each
-    // driver class will pick and choose from this set.  Use the long option name for the 
+    // driver class will pick and choose from this set.  Use the long option name for the
     // key of this hash.
     private HashMap allOpts = new HashMap();
-    
+
     // This is used to sort the options
     private OptionComparator oc;
-    
+
     // Usage into, passed to the HelpFormatter
     private String cmdLineSyntax;
     private String usageHeader;
@@ -86,14 +86,14 @@ public class App {
     private CommandLine line;
 
     // If -s or -p was given, this is the dummy input XML file:
-    private InputSource dummyXmlFile;   
+    private InputSource dummyXmlFile;
 
     // If --catalog was given, this will be the CatalogResolver
     private CatalogResolver resolver = null;
-    
+
     // If --xslt was given, this will be the transformer
     private Transformer xslt = null;
-    
+
     // If --title was given, this holds the value
     private String dtdTitle = null;
 
@@ -103,19 +103,19 @@ public class App {
     // If --params was given, this holds the keys and values in an array.  Even
     // indeces are keys, odd indeces are values.
     private String[] xsltParams = new String[0];
-    
-    
+
+
     // DtdDocumentor options, to be passed in as XSLT params
-    
+
     private String dir = null;
     private String css = null;
     private String js = null;
     private String include = null;
     private boolean suffixes = true;
     private String excludeElems = null;
-    private String excludeExcept = null;    
+    private String excludeExcept = null;
 
-    
+
     /**
      * Constructor.  The list of options should be in the same order that you want them
      * to be output in the usage message.
@@ -124,7 +124,7 @@ public class App {
         String homeStr = System.getProperty("DTDANALYZER_HOME");
         if (homeStr == null) homeStr = ".";
         home = new File(homeStr);
-        
+
         initAllOpts();
         oc = new OptionComparator(optList);
         cmdLineSyntax = _cmdLineSyntax;
@@ -136,8 +136,8 @@ public class App {
 
         // Set System properties for parsing and transforming
         if ( System.getProperty(App.SAX_DRIVER_PROPERTY) == null )
-            System.setProperty(App.SAX_DRIVER_PROPERTY, App.SAX_DRIVER_DEFAULT);     
-        
+            System.setProperty(App.SAX_DRIVER_PROPERTY, App.SAX_DRIVER_DEFAULT);
+
         if ( System.getProperty(App.TRANSFORMER_FACTORY_PROPERTY) == null )
             System.setProperty(App.TRANSFORMER_FACTORY_PROPERTY, App.TRANSFORMER_FACTORY_DEFAULT);
 
@@ -147,21 +147,21 @@ public class App {
         try {
             // parse the command line arguments
             line = clp.parse( activeOpts, args );
-        
+
             // Handle --help:
             if ( line.hasOption( "h" ) ) {
                 printUsage();
                 System.exit(0);
             }
-            
+
             // Validate options
-            
+
             // Only one of -s, -d, or -p can be given
-            if ( (line.hasOption("d") ? 1:0) + (line.hasOption("s") ? 1:0) + 
+            if ( (line.hasOption("d") ? 1:0) + (line.hasOption("s") ? 1:0) +
                  (line.hasOption("p") ? 1:0) > 1) {
                  usageError("Only one of -d, -s, or -p is allowed!");
             }
-            
+
             // Check that, if given, the argument to -d is a valid file
             if (line.hasOption("d")) {
                 File xml = new File(line.getOptionValue("d"));
@@ -182,7 +182,7 @@ public class App {
                 }
                 String publicId = line.getOptionValue("p");
                 xmlFileStr += "\n\n<root/>\n";
-                
+
                 dummyXmlFile = new InputSource(new StringReader(xmlFileStr));
             }
 
@@ -194,16 +194,16 @@ public class App {
                     System.err.println("Error: Specified catalog " + catalog.toString() + " is not a file" );
                     System.exit(1);
                 }
-                
+
                 // Set up catalog resolution
                 PMCBootStrapper bootstrapper = new PMCBootStrapper();
-                CatalogManager catalogManager = new CatalogManager(); 
+                CatalogManager catalogManager = new CatalogManager();
                 catalogManager.setIgnoreMissingProperties(true);
                 URL oasisCatalog = catalogManager.getClass().getResource(App.OASIS_DTD);
                 bootstrapper.addMapping(App.OASIS_PUBLIC_ID, oasisCatalog.toString());
                 catalogManager.setBootstrapResolver(bootstrapper);
                 catalogManager.setCatalogFiles(catalog.toString());
-                resolver = new CatalogResolver(catalogManager); 
+                resolver = new CatalogResolver(catalogManager);
             }
 
             // Check for, and handle, the --xsl option
@@ -237,7 +237,7 @@ public class App {
 
             // Check for and deal with the comment processor options
             if (line.hasOption("m")) {
-                SComment.setCommentProcessor("pandoc");            
+                SComment.setCommentProcessor("pandoc");
             }
             else if (line.hasOption("docproc")) {
                 SComment.setCommentProcessor(line.getOptionValue("docproc"));
@@ -256,7 +256,7 @@ public class App {
                     }
                 }
             }
-            
+
             // Check for and store all of the dtddocumentor options
             if (line.hasOption("dir")) {
                 dir = line.getOptionValue("dir");
@@ -285,7 +285,7 @@ public class App {
             usageError(exp.getMessage());
         }
     }
-    
+
     /**
      * Get the home directory.  This is passed in from our startup scripts as the value of
      * the DTDANALYZER_HOME system property.
@@ -307,7 +307,7 @@ public class App {
     public Options getActiveOpts() {
         return activeOpts;
     }
-    
+
     /**
      * If -s or -p were used, this should hold the dummy XML file
      */
@@ -321,7 +321,7 @@ public class App {
     public CatalogResolver getResolver() {
         return resolver;
     }
-    
+
     /**
      * If --xslt was given, this will return a Transformer object.  Even if not, this
      * will return a Transformer that does the identity transformation.
@@ -337,13 +337,13 @@ public class App {
         return dtdTitle;
     }
 
-    /** 
+    /**
      * If --roots was given, this returns the value as an array
      */
     public String[] getRoots() {
         return roots;
     }
-    
+
     /**
      * If --params was given, this returns the keys and values in an alternating array.
      */
@@ -398,7 +398,7 @@ public class App {
      */
     public String getExcludeExcept() {
         return excludeExcept;
-    }    
+    }
 
 
 
@@ -406,9 +406,9 @@ public class App {
 
     /**
      * This initializes the allOpts hash.  Note that we're using the long option
-     * name as the key to this hash.  That's required, because the array of names that 
+     * name as the key to this hash.  That's required, because the array of names that
      * each driver class passes into the constructor here as optList is used for two
-     * different things:  1, to pull the Option object out of the allOpts hash; and 
+     * different things:  1, to pull the Option object out of the allOpts hash; and
      * 2, in OptionComparator to sort the options for the usage message.  The OptionComparator
      * requires those values to match the long option names.
      *
@@ -417,7 +417,7 @@ public class App {
      */
     private void initAllOpts() {
         allOpts.put("help", new Option("h", "help", false, "Get help"));
-        allOpts.put("doc", 
+        allOpts.put("doc",
             OptionBuilder
                 .withLongOpt( "doc" )
                 .withDescription("Specify an XML document used to find the DTD. This could be just a \"stub\" " +
@@ -434,7 +434,7 @@ public class App {
                     "pathname, if the DTD exists in a file on your system, or an HTTP URL.")
                 .hasArg()
                 .withArgName("system-id")
-                .create('s') 
+                .create('s')
         );
         allOpts.put("public",
             OptionBuilder
@@ -443,7 +443,7 @@ public class App {
                     "conjunction with an OASIS catalog file.")
                 .hasArg()
                 .withArgName("public-id")
-                .create('p') 
+                .create('p')
         );
         allOpts.put("catalog",
             OptionBuilder
@@ -451,7 +451,7 @@ public class App {
                 .withDescription("Specify a file to use as the OASIS catalog, to resolve public identifiers.")
                 .hasArg()
                 .withArgName("catalog-file")
-                .create('c') 
+                .create('c')
         );
         allOpts.put("xslt",
             OptionBuilder
@@ -459,7 +459,7 @@ public class App {
                 .withDescription("An XSLT script to run to post-process the output. This is optional.")
                 .hasArg()
                 .withArgName("xslt")
-                .create('x') 
+                .create('x')
         );
         allOpts.put("title",
             OptionBuilder
@@ -468,12 +468,12 @@ public class App {
                     "element under the root <declarations> element of the output XML.")
                 .hasArg()
                 .withArgName("dtd-title")
-                .create('t') 
+                .create('t')
         );
         allOpts.put("roots",
             OptionBuilder
                 .withLongOpt("roots")
-                .withDescription("Specify the set of possible root elements for documents conforming " + 
+                .withDescription("Specify the set of possible root elements for documents conforming " +
                     "to this DTD.  These elements will be tagged with a 'root=true' attribute in " +
                     "the output.  This will also cause the program to find those elements that " +
                     "are not reachable from this set of possible root elements, and to tag those " +
@@ -513,7 +513,8 @@ public class App {
         allOpts.put("dir",
             OptionBuilder
                 .withLongOpt("dir")
-                .withDescription("Specify the directory to which to write the output files.")
+                .withDescription("Specify the directory to which to write the output files. " +
+                    "Defaults to \"doc\".")
                 .hasArg()
                 .withArgName("dir")
                 .create()
@@ -575,7 +576,7 @@ public class App {
 
 
     }
-    
+
    /**
     * Outputs usage message to standard output
     */
@@ -593,7 +594,7 @@ public class App {
         formatter.printHelp(new PrintWriter(os, true), 80, cmdLineSyntax, usageHeader, activeOpts,
             2, 2, "");
     }
-    
+
     /**
      * Use this when there's a serious error in the command line
      */
