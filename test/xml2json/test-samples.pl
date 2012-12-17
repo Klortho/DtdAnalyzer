c00ca56b83cb925de4232b1c8e69e63947f22936
Chris Maloney <voldrani@gmail.com>
2012-12-17 17:22:46 -0500

    Adding a simple test script

---
+++ test/xml2json/test-samples.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+# This rudimentary test script does the following:
+# * Runs make-samples.sh, which conversion XSLTs and generates JSON files for each
+#   of the samples
+# * Compares each of the resultant JSON files with the reference one that is in
+#   git, and fails if they are different
+# * As an added check, runs each resultant JSON through jsonlint
+#  (https://github.com/zaach/jsonlint), and fails if there are syntax errors.
+
+
+use strict;
+use File::Compare;
+
+my @samples = qw(
+    sample1 sample2a sample2b sample3 sample4
+);
+my $failpref = "****** Failed: ";
+
+print "Generating outputs for each of the samples.\n";
+#system "./make-samples.sh";
+
+
+
+print "Testing that each expected output exists.\n";
+foreach my $s (@samples) {
+    my $sjson = $s . ".json";
+    die "$failpref $sjson not found." if (!-f $sjson);
+}
+
+print "Testing that the new outputs are the same as the reference outputs.\n";
+foreach my $s (@samples) {
+    my $sjson = $s . ".json";
+    my $srefjson = $s . ".ref.json";
+    if (compare($sjson, $srefjson) != 0) {
+        die "$failpref  New generated $sjson is not the same as refence $srefjson.";
+    }
+}
+
+print "Testing that each new output is valid JSON.\n";
+foreach my $s (@samples) {
+    my $sjson = $s . ".json";
+    my $jsonlint_cmd = "jsonlint $sjson";
+    system($jsonlint_cmd . " > /dev/null 2>\&1");
+    if ($? != 0) {
+        die "$failpref $sjson does not appear to be valid JSON.\nTry '$jsonlint_cmd'.\n";
+    }
+}
+
