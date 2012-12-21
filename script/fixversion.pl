e72648beb339b122edd8c62759d165b5a76df1f6
Chris Maloney <voldrani@gmail.com>
2012-12-20 22:40:23 -0500

    Wrote some scripts to automate release

--- script/fixversion.pl
+++ script/fixversion.pl
@@ -1,14 +1,135 @@
 #!/usr/bin/perl
-
 # This script does the following
 #   * Modifies the version number in the following files:
 #       * build.xml
-#       * README.md - in various hyperlinks
 #       * ReleaseNotes.md
+#   * If the argument is a new release version number:
+#       * README.md - links to the latest release
 #   * Removes any left over DtdAnalyzer-*{.zip|.tar.gz} files (in order to
 #     make sure these don't get stuffed into the next .zip|tar.gz set).
 #   * Removes left over lib/DtdAnalyzer-*.jar
 # If the version given is 'dev', then this also:
 #   * Creates a new template section at the top of ReleaseNotes.md.
 
+use strict;
+
+my $arg = $ARGV[0];
+if (!$arg) {
+    die "Need a version number or 'dev', please.\n";
+}
+if ($arg ne "dev" && $arg !~ /^\d+(\.\d+)+$/) {
+    die "That doesn't look like a version number to me.\n";
+}
+my $switchToDev = ($arg eq "dev");
+
+# Fix files
+my ($old, $new);
+
+# Fix build.xml
+print "Fixing build.xml\n";
+$old = "build.xml";
+$new = "$old.tmp";
+open(OLD, "< $old") or die "Can't open $old: $!";
+open(NEW, "> $new") or die "Can't open $new for writing: $!";
+
+my $oldVersion;
+my $newVersion;
+while (<OLD>) {
+    if (/(\<property name="version" value=\")(.*?)(\"\/\>)/) {
+        $oldVersion = $2;
+        if ($switchToDev && $oldVersion !~ /^\d+(\.\d+)+$/) {
+            die "Old version '$oldVersion' doesn't look like a release version number.\n" .
+                "Are you sure you meant to use 'dev'?\n";
+        }
+        if (!$switchToDev && $oldVersion !~ /-dev$/) {
+            die "Old version doesn't look like a dev version to me.  Something's not right.\n";
+        }
+        $newVersion = $switchToDev ? $oldVersion . "-dev" : $arg;
+        print "Changing version number from $oldVersion -> $newVersion.\n";
+        s/(\<property name="version" value=\")(.*?)(\"\/\>)/$1$newVersion$3/;
+    }
+    (print NEW $_) or die "Can't write to $new: $!";
+}
+close(OLD) or die "Can't close $old: $!";
+close(NEW) or die "Can't close $new: $!";
+#unlink $old or die "Can't remove $old: $!";
+#rename($new, $old) or die "can't rename $new to $old: $!";
+
+if (!$oldVersion) {
+    die "Couldn't find a valid version number in build.xml.\n";
+}
+
+# Fix README.md
+my $lastReleaseVer;
+if (!$switchToDev) {
+    print "Fixing README.\n";
+    $old = "README.md";
+    $new = "$old.tmp";
+    open(OLD, "< $old") or die "Can't open $old: $!";
+    open(NEW, "> $new") or die "Can't open $new for writing: $!";
+
+    while (<OLD>) {
+        #print $_;
+        if (/(DtdAnalyzer-)(\d+(\.\d+)+)(\.zip|\.tar\.gz)/) {
+            $lastReleaseVer = $1;
+        }
+        if (!$switchToDev) {
+            s/(DtdAnalyzer-)(\d+(\.\d+)+)(\.zip|\.tar\.gz)/$1$newVersion$4/g;
+        }
+        (print NEW $_) or die "Can't write to $new: $!";
+    }
+    close(OLD) or die "Can't close $old: $!";
+    close(NEW) or die "Can't close $new: $!";
+    #unlink $old or die "Can't remove $old: $!";
+    #rename($new, $old) or die "can't rename $new to $old: $!";
+}
+
+# Fix ReleaseNotes.md
+# If switching to dev, then write a new template at the top; otherwise,
+# just change the version number from old -> new
+my $relNotesTemplate = <<RELTEMPL;
+
+## DtdAnalyzer v$newVersion
+
+### Features
+
+### Enhancements
+
+### Bug fixes
+
+### Performance
+
+### Other
+RELTEMPL
+
+print "Fixing ReleaseNotes.md.\n";
+$old = "ReleaseNotes.md";
+$new = "$old.tmp";
+open(OLD, "< $old") or die "Can't open $old: $!";
+open(NEW, "> $new") or die "Can't open $new for writing: $!";
+
+while (<OLD>) {
+    if ($switchToDev) {
+        if (/\# DtdAnalyzer Release Notes/) {
+            (print NEW $_) or die "Can't write to $new: $!";
+            (print NEW $relNotesTemplate) or die "Can't write to $new: $!";
+        }
+        else {
+            (print NEW $_) or die "Can't write to $new: $!";
+        }
+    }
+    else {
+        s/\bv$oldVersion\b/v$newVersion/;
+        (print NEW $_) or die "Can't write to $new: $!";
+    }
+}
+close(OLD) or die "Can't close $old: $!";
+close(NEW) or die "Can't close $new: $!";
+#unlink $old or die "Can't remove $old: $!";
+#rename($new, $old) or die "can't rename $new to $old: $!";
+
+# Get rid of old DtdAnalyzer-*.zip etc. files
+unlink glob "DtdAnalyzer-*.zip";
+unlink glob "DtdAnalyzer-*.tar.gz";
+unlink glob "lib/DtdAnalyzer-*.jar";
 
--- script/push-website.pl
+++ script/push-website.pl
@@ -1,6 +1,93 @@
 #!/usr/bin/perl
-
 # This script:
-#   * Copies the newly build .zip and .tar.gz files into website/downloads,
+#   * Moves the newly build .zip and .tar.gz files into website/downloads,
+#   * Updates website/index.html to point to these
 #   * Updates the website/downloads/index.html file to add these binaries to
 #     the list.
+
+use strict;
+use File::Copy 'move';
+
+my $zipFile;
+my $version;
+foreach my $f (<DtdAnalyzer-*.zip>) {
+    # Do sanity checking to make sure there is only one, and that it's version
+    # number looks like a release version number
+    if ($zipFile) {
+        die "Found more than one zip file.\n";
+    }
+    if ($f !~ m/DtdAnalyzer-(\d(\.\d)+)\.zip/) {
+        die "Zip filename doesn't look like a release binary.\n";
+    }
+    $zipFile = $f;
+    $version = $1;
+}
+if (!$zipFile) {
+    die "Couldn't find any zip file.\n";
+}
+
+my $gzFile;
+foreach my $f (<DtdAnalyzer-*.tar.gz>) {
+    if ($gzFile) {
+        die "Found more than one gz file.\n";
+    }
+    if ($f !~ m/DtdAnalyzer-(\d(\.\d)+)\.tar.gz/) {
+        die "GZ filename doesn't look like a release binary.\n";
+    }
+    if ($version ne $1) {
+        die "Tar and GZ version numbers don't match!\n";
+    }
+    $gzFile = $f;
+}
+if (!$gzFile) {
+    die "Couldn't find any gz file.\n";
+}
+
+# Now move them:
+print "Found zip and gz version $version, moving to website/downloads.\n";
+#move $zipFile, 'website/downloads';
+#move $gzFile, 'website/downloads';
+
+# Fix website/index.html
+my ($old, $new);
+$old = "website/index.html";
+print "Fixing $old\n";
+$new = "$old.tmp";
+open(OLD, "< $old") or die "Can't open $old: $!";
+open(NEW, "> $new") or die "Can't open $new for writing: $!";
+while (<OLD>) {
+    s/DtdAnalyzer-\d(\.\d)+/DtdAnalyzer-$version/g;
+    (print NEW $_) or die "Can't write to $new: $!";
+}
+close(OLD) or die "Can't close $old: $!";
+close(NEW) or die "Can't close $new: $!";
+#unlink $old or die "Can't remove $old: $!";
+#rename($new, $old) or die "can't rename $new to $old: $!";
+
+# Fix website/downloads/index.html
+$old = "website/downloads/index.html";
+print "Fixing $old\n";
+$new = "$old.tmp";
+open(OLD, "< $old") or die "Can't open $old: $!";
+open(NEW, "> $new") or die "Can't open $new for writing: $!";
+while (<OLD>) {
+    if (/\<h2\>Latest release\<\/h2\>/) {
+        (print NEW $_) or die "Can't write to $new: $!";
+        print NEW "    <p>$version</p>\n" .
+                  "    <ul>\n" .
+                  "      <li><a href='DtdAnalyzer-$version.tar.gz'>DtdAnalyzer-$version.tar.gz</a></li>\n" .
+                  "      <li><a href='DtdAnalyzer-$version.zip'>DtdAnalyzer-$version.zip</a></li>\n" .
+                  "    </ul>\n" .
+                  "    <h2>Old releases</h2>\n";
+    }
+    elsif (/\<h2\>Old releases\<\/h2\>/) {
+        # do nothing
+    }
+    else {
+        (print NEW $_) or die "Can't write to $new: $!";
+    }
+}
+close(OLD) or die "Can't close $old: $!";
+close(NEW) or die "Can't close $new: $!";
+#unlink $old or die "Can't remove $old: $!";
+#rename($new, $old) or die "can't rename $new to $old: $!";
